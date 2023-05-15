7e9c67a424e9795105067bdc13260c9ee3f9dd0b
Matt Bulfair <Matt_Bulfair@cable.comcast.com>
2023-05-15 16:30:55 -0400

    custom server setup option

--- packages/next-13-demo/server.ts
+++ packages/next-13-demo/server.ts
@@ -2,7 +2,7 @@ import { createServer } from "http";
 import { parse } from "url";
 import next from "next";
 // Import Stencil's Hydrate
-import { renderToString } from '@matt/stencil-components/hydrate'
+import { renderToString } from "@matt/stencil-components/hydrate";
 
 const port = parseInt(process.env.PORT || "3000", 10);
 const dev = process.env.NODE_ENV !== "production";
@@ -10,9 +10,29 @@ const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  createServer((req, res) => {
+  createServer(async (req, res) => {
     const parsedUrl = parse(req.url!, true);
-    handle(req, res, parsedUrl);
+    // Get HTML
+    const html = await app.renderToHTML(
+      req,
+      res,
+      parsedUrl.pathname!,
+      parsedUrl.query
+    );
+    // Stencil
+    const renderedHtml = await renderToString(html, {
+      prettyHtml: false,
+      removeHtmlComments: true,
+      clientHydrateAnnotations: false,
+    });
+    // THIS IS WHAT WILL BREAK
+    if (parsedUrl.pathname?.startsWith("/_next") || parsedUrl.pathname?.startsWith("/__next")) {
+      await handle(req, res, parsedUrl);
+    } else {
+      res.end(renderedHtml.html);
+    }
+    // THIS IS WHAT IS DEFAULT
+    // handle(req, res, parsedUrl);
   }).listen(port);
 
   console.log(
